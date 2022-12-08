7231a4fd16ad8965e7e388b98e53bf6815301428
Simone <simon.red.au@gmail.com>
2022-12-08 18:24:28 +0100

    added contdown

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,73 @@
 import React from "react";
 import Countdown from "react-countdown";
+import "./App.css";
+
+const Completionist = () => <span>You are good to go!</span>;
+
+const Box = ({ children, text }) => {
+  return (
+    <div className="numbers">
+      <div className="number-value">{children}</div>
+      <div className="number-text">{text}</div>
+    </div>
+  );
+};
+
+const renderer = ({ days, hours, minutes, seconds, completed }) => {
+  const boxes = [
+    {
+      text: "dia",
+      value: days,
+    },
+    {
+      text: "min",
+      value: minutes,
+    },
+    {
+      text: "horas",
+      value: hours,
+    },
+    {
+      text: "segundos",
+      value: seconds,
+    },
+  ];
+
+  if (completed) {
+    // Render a complete state
+    return <Completionist />;
+  } else {
+    // Render a countdown
+    return (
+      <>
+        <h1>MAKIVERSARIO en</h1>
+        {boxes.map(({ text, value }) => {
+          return (
+            <Box key={text} text={text}>
+              {value}
+            </Box>
+          );
+        })}
+      </>
+    );
+  }
+};
+
+const Counter = () => {
+  return (
+    <Countdown
+      date={new Date("December 10, 2022 00 :00:00")}
+      renderer={renderer}
+      daysInHours={true}
+    />
+  );
+};
 
 export function App() {
   return (
     <div className="App">
       <main>
-        <Countdown date={Date.now() + 100000} />
+        <Counter />
       </main>
     </div>
   );
